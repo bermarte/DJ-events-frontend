79b1bca6ed4e1d8ca41634523921627b72f44058
Bernardo Martelli <bernardo.martelli@gmail.com>
2023-07-10 17:36:34 +0200

    EventItem component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
 }
