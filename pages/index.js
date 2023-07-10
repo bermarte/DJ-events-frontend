7c2d6d7ce2533ed0fce67602f0e546ec7976fcbb
Bernardo Martelli <bernardo.martelli@gmail.com>
2023-07-10 16:31:38 +0200

    use next's getServersideProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Layout from "@/components/Layout";
-export default function HomePage() {
+import { API_URL } from "@/config/index";
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+  };
 }
